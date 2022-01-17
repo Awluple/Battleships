6293228f9bd3c6504a187450c36f9f49eddbd310
Awluple <cdqwerty1@gmail.com>
2022-01-17 05:19:24 +0000

    Init

---
+++ controllers/Test.xaml.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Battleships
+{
+    public partial class TestPage : Page
+    {
+        public TestPage()
+        {
+            InitializeComponent();
+        }
+
+        void hyperlink_Click(object sender, RoutedEventArgs e) {
+            Uri uri = new Uri("../views/Test2.xaml", UriKind.Relative);
+            // this.NavigationService.Navigate(uri);
+            
+        }
+    }
+}
